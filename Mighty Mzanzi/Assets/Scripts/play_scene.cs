a75ab258e213cf25a31dc79b4c8acdb8ac2c7444
Erin Hutton <eringracehutton@gmail.com>
2022-11-03 21:35:07 +0200

    Changed build setting to preload game scene, removed my scrapped scripts

--- Mighty Mzanzi/Assets/Scripts/play_scene.cs
+++ Mighty Mzanzi/Assets/Scripts/play_scene.cs
@@ -4,13 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class play_scene : MonoBehaviour
-{
-    
-    public void LoadGame(int SceneIndex)
-    {
-        game_loader.instance.LoadGame(SceneIndex);
-    }
-    
+{    
     public void LoadByIndex(int SceneIndex)
     {
         SceneManager.LoadScene(SceneIndex);
