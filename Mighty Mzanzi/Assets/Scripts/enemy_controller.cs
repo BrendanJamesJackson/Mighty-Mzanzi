863cda6429c5cd743eee5f4e33829184ffc95447
Erin Hutton <eringracehutton@gmail.com>
2022-11-10 12:13:42 +0200

    Started health bar stuff

--- Mighty Mzanzi/Assets/Scripts/enemy_controller.cs
+++ Mighty Mzanzi/Assets/Scripts/enemy_controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemy_controller : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class enemy_controller : MonoBehaviour
     public int enemy_bonus;
 
     public game_manager gm_script;
+    private Image healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         gm_script = GameObject.FindGameObjectWithTag("GameManager").GetComponent<game_manager>();
-
+        healthBar = GetComponentInChildren<Image>(false);
+        
     }
 
     private void Update()
